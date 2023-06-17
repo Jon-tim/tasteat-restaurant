15875e3d7d59a9ab6f67e5534cc8170d8710991a
Oluwatimilehin Awoniyi <awoniyitimi@outlook.com>
2023-06-17 20:17:29 +0100

    feat: mobile- Add operations details

--- components/molecules/History.jsx
+++ components/molecules/History.jsx
@@ -1,7 +1,44 @@
-
+import { operations } from "@/data/operations";
 
 export default function History() {
-  return (
-	<section className="bg-faded  px-4 py-8">History</section>
-  )
+	return (
+		<section className="bg-faded  px-4 py-8">
+			<section className="flex flex-col gap-4">
+				{operations.map((item, id) => (
+					<div
+						key={item.id}
+						className="capitalize flex items-center flex-col "
+					>
+						<div className="bg-dark-accent w-max p-3 rounded-full">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="24"
+								height="24"
+								viewBox="0 0 24 24"
+								fill="white"
+							>
+								{item.r && (
+									<circle
+										cx="12"
+										cy="12"
+										r="4"
+									></circle>
+								)}
+								<path d={item.path}></path>
+							</svg>
+						</div>
+						<h4 className="text-dark-accent text-lg">
+							{item.title}
+						</h4>
+						<p className="text-sm text-text-color">
+							{item.details}
+						</p>
+					</div>
+				))}
+			</section>
+			<section>
+				
+			</section>
+		</section>
+	);
 }
---
+++ data/operations.js
@@ -0,0 +1,21 @@
+export const operations = [
+	{
+		id: 1,
+		r: true,
+		path: "M13 4.069V2h-2v2.069A8.01 8.01 0 0 0 4.069 11H2v2h2.069A8.008 8.008 0 0 0 11 19.931V22h2v-2.069A8.007 8.007 0 0 0 19.931 13H22v-2h-2.069A8.008 8.008 0 0 0 13 4.069zM12 18c-3.309 0-6-2.691-6-6s2.691-6 6-6 6 2.691 6 6-2.691 6-6 6z",
+		title: "locate us",
+		details: "riverside 25,san diego, califonia",
+	},
+	{
+		id: 2,
+		path: "M5 5v14a1 1 0 0 0 1 1h3v-2H7V6h2V4H6a1 1 0 0 0-1 1zm14.242-.97-8-2A1 1 0 0 0 10 3v18a.998.998 0 0 0 1.242.97l8-2A1 1 0 0 0 20 19V5a1 1 0 0 0-.758-.97zM15 12.188a1.001 1.001 0 0 1-2 0v-.377a1 1 0 1 1 2 .001v.376z",
+		title: "open hours",
+		details: "mon to fri 9:00 AM - 9:00 PM",
+	},
+	{
+		id: 3,
+		path: "M19 4h-3V2h-2v2h-4V2H8v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zm-7 10H7v-2h5v2zm5-4H7V8h10v2z",
+		title: "reservation",
+		details: "tasteat@gmail.com",
+	},
+];
